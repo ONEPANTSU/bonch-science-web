a5a8b428541e3740cbb078eb2687232ede977670
psina <zheka.evlanov@yandex.ru>
2021-12-09 13:50:50 +0300

    SEC

---
+++ src/main/java/BSWeb/repo/SECAchievmentsRepository.java
@@ -0,0 +1,7 @@
+package BSWeb.repo;
+
+import BSWeb.models.Achievments;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SECAchievmentsRepository extends CrudRepository<Achievments, Long> {
+}
