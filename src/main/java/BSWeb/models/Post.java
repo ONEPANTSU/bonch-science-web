b5e3b7078ca465996d9209a62b10ea234c55b84b
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2021-12-10 02:53:14 +0300

    Фикс бага
    https://stackoverflow.com/questions/49813666/table-dbname-hibernate-sequence-doesnt-exist

--- src/main/java/BSWeb/models/Post.java
+++ src/main/java/BSWeb/models/Post.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String text;
