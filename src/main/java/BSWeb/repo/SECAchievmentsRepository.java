d4b7afc16719d127d67f8b1e4e30dcd90e89a0c1
Saimon <saimon22011@gmail.com>
2021-12-10 19:05:05 +0300

    Auto stash before rebase of "origin/main"

--- src/main/java/BSWeb/repo/SECAchievmentsRepository.java
+++ src/main/java/BSWeb/repo/SECAchievmentsRepository.java
@@ -1,7 +1,9 @@
 package BSWeb.repo;
 
 import BSWeb.models.Achievments;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SECAchievmentsRepository extends CrudRepository<Achievments, Long> {
+    
 }
--- src/main/java/BSWeb/repo/SECRepository.java
+++ src/main/java/BSWeb/repo/SECRepository.java
@@ -1,6 +1,7 @@
 package BSWeb.repo;
 
 import BSWeb.models.SEC;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SECRepository extends CrudRepository<SEC, Long> {
