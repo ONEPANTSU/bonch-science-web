5058a4e122ed927ee7dbbc1887be99892f60e277
Artyom <quartyom@gmail.com>
2021-12-17 20:15:17 +0300

    AddedSessionAttributes

--- src/main/java/BSWeb/controllers/admin/PanelController.java
+++ src/main/java/BSWeb/controllers/admin/PanelController.java
@@ -1,63 +1,72 @@
 package BSWeb.controllers.admin;
 
+import BSWeb.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.security.auth.login.LoginContext;
-import java.lang.reflect.Field;
-import java.sql.*;
-import java.util.ArrayList;
-
 
 @Controller
 @RequestMapping("/admin/panel")
 public class PanelController {
     @Autowired
-    LoginController loginController;
+    User user;
 
     @GetMapping("")
     public String panel(Model model)
     {
-        if (loginController.getAccess_level() != null && (Integer) loginController.getAccess_level() == 0){
+        if (user.getAccess_level() == null){
+            return "redirect:/admin/login";
+        }
+        else if ((Integer) user.getAccess_level() == 0){
             return "admin/panelPage";
         }
         else {
-            return "redirect:/admin/login";
+            return "redirect:/news";
         }
     }
 
     @GetMapping("/events")
     public String events(Model model)
     {
-        if (loginController.getAccess_level() != null && (Integer) loginController.getAccess_level() == 0){
+        if (user.getAccess_level() == null){
+            return "redirect:/admin/login";
+        }
+        else if ((Integer) user.getAccess_level() == 0){
             return "admin/panel/eventsPage";
         }
         else {
-            return "redirect:/admin/login";
+            return "redirect:/news";
         }
     }
 
     @GetMapping("/news")
     public String news(Model model)
     {
-        if (loginController.getAccess_level() != null && (Integer) loginController.getAccess_level() == 0){
+        if (user.getAccess_level() == null){
+            return "redirect:/admin/login";
+        }
+        else if ((Integer) user.getAccess_level() == 0){
             return "admin/panel/newsPage";
         }
         else {
-            return "redirect:/admin/login";
+            return "redirect:/news";
         }
     }
+
     @GetMapping("/scientific-and-educational-centers")
     public String SECs(Model model)
     {
-        if (loginController.getAccess_level() != null && (Integer) loginController.getAccess_level() == 0){
+        if (user.getAccess_level() == null){
+            return "redirect:/admin/login";
+        }
+        else if ((Integer) user.getAccess_level() == 0){
             return "admin/panel/secPage";
         }
         else {
-            return "redirect:/admin/login";
+            return "redirect:/news";
         }
     }
 }
