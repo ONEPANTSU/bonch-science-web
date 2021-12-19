59fca60481599145ac837b3d12ac143fec2376ba
psina <zheka.evlanov@yandex.ru>
2021-12-19 15:51:44 +0300

    Список мероприятий и регистрация на них

--- src/main/java/BSWeb/models/Event.java
+++ src/main/java/BSWeb/models/Event.java
@@ -10,4 +10,33 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String title;
+    private String text;
+    private String date;
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
 }
