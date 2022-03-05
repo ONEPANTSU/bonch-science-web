f4038260cc17bcdc763bb5a37f01540479461f90
ONEPANTSU <conandet@mail.ru>
2022-03-05 18:23:14 +0300

    Redirect to home + .war

--- src/main/java/BSWeb/controllers/MainController.java
+++ src/main/java/BSWeb/controllers/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Контроллер главной страницы
@@ -11,6 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public RedirectView redirectToHome(RedirectAttributes attributes) {
+        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectAttributes");
+        attributes.addAttribute("attribute", "redirectWithRedirectAttributes");
+        return new RedirectView("/home");
+    }
+
 
     @GetMapping("/home")
     public String greeting(Model model) {
